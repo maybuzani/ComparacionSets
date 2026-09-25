c1164d0fa4c93f21e7e85983a40484daa543aba2
Tu Nombre <tu-correo@example.com>
2026-09-25 13:33:21 -0700

    Trabajar con HashSet

--- src/EjemploHashSet.java
+++ src/EjemploHashSet.java
@@ -1,2 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class EjemploHashSet {
+    public static void main(String[] args) {
+        Set<String> tecnologias = new HashSet<>();
+
+        tecnologias.add("Java");
+        tecnologias.add("Python");
+        tecnologias.add("JavaScript");
+        tecnologias.add("Java");
+        tecnologias.add("SQL");
+        tecnologias.add("Python");
+        tecnologias.add("Git");
+        tecnologias.add("Docker");
+
+        System.out.println(tecnologias);
+
+        boolean agregado = tecnologias.add("Java");
+        System.out.println("¿Se agregó Java? " + agregado);
+
+        boolean agregado2 = tecnologias.add("Kotlin");
+        System.out.println("¿Se agregó Kotlin? " + agregado2);
+
+        tecnologias.contains("Java");
+        tecnologias.contains("C++");
+        tecnologias.remove("Git");
+        tecnologias.size();
+        tecnologias.isEmpty();
+        tecnologias.clear();
+
+        for (String tecnologia : tecnologias) {
+            System.out.println(tecnologia);
+        }
+    }
 }
